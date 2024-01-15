82390554adf664db7b80c2410c9527663775b7af
iliyaskhan35 <47298476+iliyaskhan35@users.noreply.github.com>
2024-01-15 17:51:12 +0000

    datatype

---
+++ .devcontainer/01_basic/04_comparesion.js
@@ -0,0 +1,21 @@
+// console.log(2>1);  //output true
+// console.log(2>=1); // output true
+// console.log(2<1); // false
+// console.log(2==1); //false
+// console.log(2!=1); // true
+
+
+// console.log("2" > 1);  //output true
+// console.log("02" > 1); // output true
+
+// console.log(null > 0)  // false
+// console.log( null ==0) //false
+// console.log (null>=0) //true
+
+
+// console.log(undefined ==0) // false
+// console.log(undefined > 0) //false
+// console.log(undefined < 0) //false
+
+
+console.log("2" ===2); // output false   because datatype difference 
