40eac5ce06c493b2f8ab2d5fd39d73a5e0144016
iliyaskhan35 <47298476+iliyaskhan35@users.noreply.github.com>
2024-02-04 17:53:28 +0000

    function

---
+++ .devcontainer/03_basic/01_function.js
@@ -0,0 +1,13 @@
+// function saymyName(){
+//     console.log("s")
+//     console.log("a")
+//     console.log("m")
+//     console.log("e")
+//     console.log("e")
+//     console.log("r")
+// }
+// saymyName()
+
+function AddNum(){
+
+}
